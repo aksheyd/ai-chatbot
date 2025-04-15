bdb4fc6fc2707ff0a9734db240907cab37065fee
Akshey D <aksheyd@umich.edu>
2025-04-14 22:00:30 -0400

    Update message-editor.tsx

--- components/message-editor.tsx
+++ components/message-editor.tsx
@@ -4,6 +4,7 @@ import { ChatRequestOptions, Message } from 'ai';
 import { Button } from './ui/button';
 import { Dispatch, SetStateAction, useEffect, useRef, useState } from 'react';
 import { Textarea } from './ui/textarea';
+import { deleteTrailingMessages } from '@/app/(chat)/actions';
 import { UseChatHelpers } from '@ai-sdk/react';
 
 export type MessageEditorProps = {
@@ -70,9 +71,9 @@ export function MessageEditor({
           onClick={async () => {
             setIsSubmitting(true);
 
-            // await deleteTrailingMessages({
-            //   id: message.id,
-            // });
+            await deleteTrailingMessages({
+              id: message.id,
+            });
 
             // @ts-expect-error todo: support UIMessage in setMessages
             setMessages((messages) => {
